8e43861303801d88a004197e967010bf241d30df
BRIGHT <BRIGHT@DESKTOP-ST51CUU>
2024-01-20 09:49:56 +0300

    Add project files.

---
+++ Sleep(Thread)/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Derleyiciyi belirli bir zaman bekletmek için aşağıdaki komutu kullanabilirsiniz.
+
+for (int i = 0; i < 100; i++)
+{
+    Console.WriteLine(i);
+
+
+    //Thead.Sleep metodu derleyiciyi belirli bir süre bekletmek için kullanılır
+    //MEtodun içerisi milisaniye cinsinden değer almaktadır. 
+
+    Thread.Sleep(1000); // 1000 milisaniye 1 saniye eder
+}
+
